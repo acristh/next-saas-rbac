741ea2a0683480ad28840a2d9989ef8adea7e9ff
Andre Cristiano <andre.cristiano1975@gmail.com>
2024-04-24 08:59:31 -0300

    feat: create permissions by role

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,2 +1,30 @@
-const teste = "oi";
+import {
+  AbilityBuilder,
+  CreateAbility,
+  MongoAbility,
+  createMongoAbility
+} from '@casl/ability'
+import { User } from './models/user'
+import { permissions } from './permissions'
+import { ProjectSubject } from './subjects/project'
+import { UserSubject } from './subjects/user'
 
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+export function defineAbilityForUser(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if(typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permission for role ${user.role} not found`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+  
+}
