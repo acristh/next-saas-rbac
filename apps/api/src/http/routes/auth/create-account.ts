102b66a6a4cbab2c243d23b941e383173aed39dd
Andre Cristiano <andre.cristiano1975@gmail.com>
2024-04-26 09:14:10 -0300

    setup: configure Swager for document the API

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -7,6 +7,8 @@ import { z } from 'zod'
 export async function createAccount(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/users',{
         schema: {
+            tags: ['auth'],
+            summary: 'Create a new user account.',
             body: z.object({
                 name: z.string(),
                 email: z.string().email(),
